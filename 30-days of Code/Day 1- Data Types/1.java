b6b545fc62f518d8e752a44726921986aa2613da
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2018-10-05 20:14:50 +0530

    Create 1.java

---
+++ 30-days of Code/Day 1- Data Types/1.java
@@ -0,0 +1,33 @@
+ Scanner sc = new Scanner(System.in);
+     /* Declare second integer, double, and String variables. */
+        int  a;
+        double b;
+        String s1 = new String() ;
+
+        /* Read and save an integer, double, and String to your variables.*/
+        a = sc.nextInt();
+        b = sc.nextDouble();
+        sc.nextLine();
+        s1 = sc.nextLine();
+        
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        
+        /* Print the sum of both integer variables on a new line. */
+            int x;
+            double y;
+      
+            x = a + i;
+            
+            System.out.println(x);
+
+        /* Print the sum of the double variables on a new line. */
+        y = d + b;
+        System.out.println(y);
+
+        
+        
+        /* Concatenate and print the String variables on a new line; 
+            the 's' variable above should be printed first. */
+        
+         System.out.println(s + s1);   
