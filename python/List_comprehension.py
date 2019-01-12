8dc940c49f9b421b8d0d3fdc224ce1f8a00ee7f6
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2019-01-12 21:42:35 +0530

    Create List_comprehension.py

---
+++ python/List_comprehension.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+result=[]
+
+result=[[x1,y1,z1] for x1 in range(0,x+1) for y1 in range(0,y+1) for z1 in range(0,z+1) if x1+y1+z1!=n ]
+
+
+print(result)                
+                    
+
