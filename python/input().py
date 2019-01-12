09d4f8a5bc4b3f44bca38a9441defe1d14d4abfc
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2019-01-12 23:51:23 +0530

    Create input().py

---
+++ python/input().py
@@ -0,0 +1,5 @@
+user_input=input().split()     # taking input with space seperated inputs and storing in user_input list
+x=int(user_input[0]) # assigning value of x 
+print(eval(input())==int(user_input[1]))     #taking polynomial input() in eval() and comparing ; polynomial eg: x**3+x**2+x+1
+
+
